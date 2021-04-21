71f5959857bf1e46f1eeb8b6b6494711416ddd5b
willamy388720 <willamygt@gmail.com>
2021-04-21 11:12:36 -0300

    Add Gender To Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_134353) do
+ActiveRecord::Schema.define(version: 2021_04_21_140938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_134353) do
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "gender"
   end
 
 end
